1aa044ca4ac6fd053d65e16dd098f23a40f04982
Saurabh Bhosale <61474697+saurabh-ctrl@users.noreply.github.com>
2020-11-04 22:06:06 +0530

    Update sync_w2r.v

--- sync_w2r.v
+++ sync_w2r.v
@@ -1,6 +1,6 @@
 // 2. sync_w2r module define:
 // Purpose: Make the read pointer(rptr) synchronized with write clock domain(wclk). 
-// 			Synchronnized rptr(wq2_rptr) used in fifo_full operation.
+// 	    Synchronnized rptr(wq2_rptr) used in fifo_full operation.
 module sync_w2r( rq2_wptr,wptr,rclk,rrstn);
 
 	// Parameter Define:
@@ -8,11 +8,11 @@ module sync_w2r( rq2_wptr,wptr,rclk,rrstn);
 	
 	// Port Define:
 	input		[ADDR_WIDTH : 0]	wptr;
-	input							rclk,rrstn;
+	input					rclk,rrstn;
 	output	reg	[ADDR_WIDTH : 0]	rq2_wptr;
 	
 	// Internal Variable:
-	reg			[ADDR_WIDTH : 0]	rq1_wptr;
+	reg		[ADDR_WIDTH : 0]	rq1_wptr;
 	
 	// Code:
 	always@(posedge rclk or negedge rrstn)
