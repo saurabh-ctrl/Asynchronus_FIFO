52e9b56fb065c00253a638b7ddefa6db9371d99e
Saurabh Bhosale <61474697+saurabh-ctrl@users.noreply.github.com>
2020-11-04 22:04:36 +0530

    Update sync_r2w.v
    Make the write pointer(wptr) synchronized with read clock domain(rclk).

--- sync_r2w.v
+++ sync_r2w.v
@@ -1,6 +1,6 @@
 // 1. sync_r2w module define:	
 // Purpose: Make the write pointer(wptr) synchronized with read clock domain(rclk). 
-// 			Synchronnized wptr(rq2_wptr) used in fifo_empty operation.
+// 	    Synchronnized wptr(rq2_wptr) used in fifo_empty operation.
 module sync_r2w( wq2_rptr,rptr,wclk,wrstn );
 	
 	// Parameter Define:
@@ -8,7 +8,7 @@ module sync_r2w( wq2_rptr,rptr,wclk,wrstn );
 	
 	// Port Define:
 	input		[ADDR_WIDTH : 0]	rptr;
-	input							wclk,wrstn;
+	input					wclk,wrstn;
 	output reg	[ADDR_WIDTH : 0]	wq2_rptr;
 	
 	// Internal Variable:
